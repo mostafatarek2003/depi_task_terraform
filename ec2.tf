2c18c53eb3ce38dbdd3a2ffcc5200b720464253b
Mostafa Tarek <mostafatarek9008@gmail.com>
2025-04-13 16:27:23 +0000

    Fresh start: cleaned repo with project files

---
+++ ec2.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "example" {
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.nano"
+  subnet_id     = aws_subnet.public_subnet.id
+
+  
+  tags = {
+    Name = "web-server-ec2"
+  }
+
+associate_public_ip_address = true
+vpc_security_group_ids = [aws_security_group.allow_http.id]
+
+user_data = <<-EOF
+              #!/bin/bash
+              sudo apt install -y apache2
+              sudo systemctl start apache2
+              echo "Hello, World" > /var/www/html./index.html
+              EOF
+}
+
+
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_https"
+  description = "Allow http inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.web_vpc.id
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" 
+}
---
+++ vpc.tf
@@ -0,0 +1,42 @@
+resource "aws_vpc" "web_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.web_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+
+
+
+
+resource "aws_internet_gateway" "web_igw" {
+  vpc_id = aws_vpc.web_vpc.id
+
+  tags = {
+    Name = "web-igw"
+  }
+}
+
+
+
+
+resource "aws_route_table" "public_web_route_table" {
+  vpc_id = aws_vpc.web_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.web_igw.id
+  }
+
+  tags = {
+    Name = "public_web_route_table"
+  }
+}
+
